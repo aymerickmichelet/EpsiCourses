8cffd3c8b73c1262233d83e040f6ab3293e8c101
aymnms <micheletaymerick@gmail.com>
2019-07-20 15:04:41 +0200

    opperationnel

---
+++ script.bat
@@ -0,0 +1,38 @@
+@echo off
+
+REM NATION (exemple: French=fr, German=ge, Europe=eu...)
+set nation=fr
+REM PSEUDO
+set pseudo=aymnms
+REM NAME OF YOU PROJECT
+set project_name=test
+REM NAME OF YOU MAINE SOURCE FILE WITHOUT FORMAT
+set main_file_name=Test
+REM Destination File (WITHOUT FILE NAME)
+set name_jar=C:\Users\aymerick\Documents\Projets\MC\serv\plugins\
+REM JavaBuildPath (path1;path2;...;)
+set classpath=.;spigot;
+
+
+
+echo Suppression du bin...
+rm -r bin
+mkdir bin
+echo Suppression du bin Done!
+
+echo Compilation...
+javac -d bin src\%nation%\%pseudo%\%project_name%\%main_file_name%.java
+echo Compilation Done!
+
+echo plugin.yml...
+cp plugin.yml bin\plugin.yml
+echo plugin.yml Done!
+
+echo Archivage...
+cd bin
+jar -cf %name_jar%\%project_name%.jar %nation%\%pseudo%\%project_name%\%main_file_name%.class plugin.yml
+cd ..
+echo Archivage Done!
+
+echo End.
+pause
---
+++ src/fr/aymnms/test/Test.java
@@ -0,0 +1,14 @@
+package fr.aymnms.test;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Test extends JavaPlugin{
+
+    public void onEnable(){
+        System.out.println("Wesh alors");
+    }
+
+    public void onDisable(){
+        System.out.println("Hola Quetal");
+    }
+}
