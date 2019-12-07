4e6d3e13c76a96fb8c243c3a95913fff2a64d525
Aymerick MICHELET <aymerick.michelet@epsi.fr>
2019-12-07 13:36:51 +0100

    Ajoutez des fichiers projet.

---
+++ StyrelDungeon/Characters/Monsters/Monster.cs
@@ -0,0 +1,26 @@
+﻿using StyrelDungeon.DungeonElements.Items;
+using System;
+using System.Collections.Generic;
+using System.Timers;
+
+namespace StyrelDungeon
+{
+    public enum MONSTER_TYPE
+    {
+        SORCERER,
+        THIEF,
+        BARBARIAN
+    }
+
+    public abstract class Monster : Character
+    {
+        protected string m_sMessageToPlayer;
+
+        public override void Attack(Character heros)
+        {
+            Console.WriteLine(m_sMessageToPlayer);
+        }
+        public override int ReceiveDamage(Character character) { return 0; }
+        public override void ApplyDamagesToEnemy(Character heros) { }
+    }
+}
