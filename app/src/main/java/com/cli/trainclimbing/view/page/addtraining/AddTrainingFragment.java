ea6df3f3b94554c2fe3835c354d62b138127b55d
Aymerick Michelet <aymerick.michelet@epsi.fr>
2021-02-06 17:33:00 +0100

    WIP create elements
    nomenclature id, layout vertical + horizontal structure

--- app/src/main/java/com/cli/trainclimbing/view/page/addtraining/AddTrainingFragment.java
+++ app/src/main/java/com/cli/trainclimbing/view/page/addtraining/AddTrainingFragment.java
@@ -23,13 +23,11 @@ public class AddTrainingFragment extends Fragment {
         homeViewModel =
                 new ViewModelProvider(this).get(AddTrainingViewModel.class);
         View root = inflater.inflate(R.layout.fragment_add_training, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+
+
+
+
         return root;
     }
 }
