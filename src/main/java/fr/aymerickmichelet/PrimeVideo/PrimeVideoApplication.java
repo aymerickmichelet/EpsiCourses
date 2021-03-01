f22a650a62015bcf6559855d7cc46a98229dfec0
TheJulienM <julien.marfella77@gmail.com>
2021-03-01 15:02:17 +0100

    Initial commit

---
+++ src/main/java/fr/aymerickmichelet/PrimeVideo/PrimeVideoApplication.java
@@ -0,0 +1,13 @@
+package fr.aymerickmichelet.PrimeVideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeVideoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/aymerickmichelet/PrimeVideo/PrimeVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.aymerickmichelet.PrimeVideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
