0656dcb9d0e605d4da89a0902a6904ff8bf6d944
TheJulienM <julien.marfella77@gmail.com>
2021-03-01 15:29:24 +0100

    ADD Eureka-server configuration
    Add application.yml, remove application.properties

--- src/main/java/fr/aymerickmichelet/primevideo/PrimeVideoApplication.java
+++ src/main/java/fr/aymerickmichelet/primevideo/PrimeVideoApplication.java
@@ -1,9 +1,11 @@
-package fr.aymerickmichelet.PrimeVideo;
+package fr.aymerickmichelet.primevideo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PrimeVideoApplication {
 
 	public static void main(String[] args) {
--- src/test/java/fr/aymerickmichelet/primevideo/PrimeVideoApplicationTests.java
+++ src/test/java/fr/aymerickmichelet/primevideo/PrimeVideoApplicationTests.java
@@ -1,4 +1,4 @@
-package fr.aymerickmichelet.PrimeVideo;
+package fr.aymerickmichelet.primevideo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
