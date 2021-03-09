852d5d80b2798dc27d34b458208542effbf6c0fa
Aymerick Michelet <aymerick.michelet@epsi.fr>
2021-03-09 22:51:06 +0100

    ADD clear + pause

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from os import system, name
 from Casino import Casino
 from math import ceil
+from time import sleep
 
 casino = Casino()
 
@@ -10,6 +11,11 @@ def clear():
         _ = system('cls')
     else:
         _ = system('clear')
+    print("-=-= Casino =-=-")
+
+
+def pause():
+    print(input("Appuyer sur <ENTREE> pour continuer..."))
 
 
 def getStringFromUser(inputText):
@@ -31,42 +37,47 @@ def getNumberFromUser(inputText, min, max):
 def menuRegister():
     global casino
     clear()
-    print("-=-= Casino =-=-")
     text = "1 - Connexion\n2 - Règle du jeu\n0 - Quitter\n"
     response = getNumberFromUser(text, 0, 2)
     if response == 1:
+        clear()
         casino.registerPlayer(getStringFromUser("Identifiant: "))
         print("Bienvenue " + casino.player.name + " !")
         return 1
     elif response == 2:
-        print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+        print("-=-= Casino =-=-")
+        print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
         print("Le but du jeu est de faire banqueroute le casino.")
-        print("Pour ce faire, vous devez gagner le plus possible. A chaque fois que vous gagnez,")
-        print("la somme gagnée est déduit du solde du casino.")
-        print("A contrario, vous perdez si votre solde est nulle.\nBonne chance !")
-        print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
+        print("Pour ce faire, vous devez gagner le plus possible.")
+        print("A chaque fois que vous gagnez, la somme obtenue est")
+        print("déduite du solde du casino.")
+        print("A contrario si votre solde est nulle, vous perdez.\nBonne chance !")
+        print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
     else:
         return 0
 
 
 def menuGame():
     global casino
-    print("-=-= Casino =-=-")
+    clear()
     infoUser = casino.player.name + " - solde: " + str(casino.player.sold) + "$"
     infoCasino = "Casino - solde: " + str(casino.sold) + "$"
     text = infoUser + "\n" + infoCasino + "\n1 - Miser sur un nombre\n0 - Deconnecter\n"
     if getNumberFromUser(text, 0, 1) == 1:
+        clear()
         # demander quel nombre il veut parier
         number = getNumberFromUser("Nombre misé [0; 49]: ", 0, 49)
         # demander combien il veut miser
         price = getNumberFromUser("Montant de la mise (< " + str(casino.player.sold) + "): ", 0, casino.player.sold)
         casino.player.withdrawalSold(price)  # retire la somme parié
         casino.settleSold(price)  # crédite la somme parié
+        clear()
         # aléatoire
         print("Lancement de la roulette...")
+        sleep(2)
         casino.roulette.startRoulette()
         print("La boule s'est arrêté sur le '" + str(casino.roulette.number) + "' !")
-        print("Calcule des gains...")
+        sleep(1)
         if casino.roulette.isSameNumber(number):
             print("Vous avez obtenu le bon nombre !\nFélicitation, vous avez gagné " + str(price * 4) + "$ !")
             casino.player.settleSold(price * 4)
@@ -77,6 +88,7 @@ def menuGame():
             casino.withdrawalSold(price * 1.5)
         else:
             print("Vous n'avez malheuresement pas gagné...")
+        pause()
     else:
         casino.resetPlayer()
 
@@ -91,12 +103,16 @@ while True:
     # regarde si player a un sold = 0
     elif casino.player.sold == 0:
         # le joueur a un sold a 0 -> perdu
+        clear()
         print("Votre solde est à 0$, le casino a gagné !")
         casino.resetPlayer()
+        pause()
 
     elif casino.sold <= 0:
+        clear()
         print("Le solde du casino est de 0$.\nFélicitation, vous avez gagné !")
         casino.resetPlayer()
+        pause()
 
     # menu
     elif casino.player is not None:
