09211c50dac353645cd338defd536baf394fce5f
Aymerick Michelet <aymerick.michelet@epsi.fr>
2021-03-10 10:58:01 +0100

    UPDATE Eurela & Webservice1 repository

---
+++ webservice1/src/main/java/fr/aymerickmichelet/webservice1/WebServiceExample.java
@@ -0,0 +1,12 @@
+package fr.aymerickmichelet.webservice1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebServiceExample {
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello";
+    }
+}
--- webservice1/src/main/java/fr/aymerickmichelet/webservice1/Webservice1Application.java
+++ webservice1/src/main/java/fr/aymerickmichelet/webservice1/Webservice1Application.java
@@ -2,8 +2,6 @@ package fr.aymerickmichelet.webservice1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Webservice1Application {
@@ -12,14 +10,4 @@ public class Webservice1Application {
 		SpringApplication.run(Webservice1Application.class, args);
 	}
 
-}
-
-//Je ne suis pas du tout sur de cette implementation de code
-
-@RestController
-public class WebServiceExample {
-	@GetMapping("/")
-	public String sayHello(){
-		return "Hello !";
-	}
 }
