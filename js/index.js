f222ebb55b35a838d33ed58dbde893070351aa30
Aymerick Michelet <aymerick.michelet@epsi.fr>
2021-03-15 17:38:47 +0100

    setup main page
    remove all sections except navigation and header, link index.js

---
+++ js/index.js
@@ -0,0 +1,26 @@
+
+
+var buttonAskJoke = document.getElementById("buttonAfkJoke");
+var buttonTellJoke = document.getElementById("buttonTellJoke");
+
+const headerMain = '<header class="masthead d-flex">\n' +
+    '    <div class="container text-center my-auto">\n' +
+    '      <h1 class="mb-1">BottleFlip</h1>\n' +
+    '      <h3 class="mb-5">\n' +
+    '        <em>Bot humour. Fait des blagues sur demande !</em>\n' +
+    '      </h3>\n' +
+    '      <a id="buttonAfkJoke" class="btn btn-primary btn-xl js-scroll-trigger" href="">Racompte moi une blague</a>\n' +
+    '      <a class="btn btn-primary btn-xl js-scroll-trigger" href="">Propose moi des blagues</a>\n' +
+    '    </div>\n' +
+    '    <div class="overlay"></div>\n' +
+    '  </header>';
+
+
+
+buttonAskJoke.onclick = function(){
+    alert("wesh alors");
+};
+
+buttonTellJoke.onclick = function(){
+    alert("wesh alors");
+};
