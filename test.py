02b28778e5959497e8da27a7036ccd49fa8c7c91
Aymerick Michelet <aymerick.michelet@epsi.fr>
2021-03-16 16:02:40 +0100

    ADD refactor test_parseInputActionAdd

--- test.py
+++ test.py
@@ -5,3 +5,8 @@ from Task import Task
 def test_parseEmptyInput():
     tm = TaskManager("")
     assert tm.action.type is None
+
+
+def test_parseInputActionAdd():
+    tm = TaskManager("+ Acheter du pain")
+    assert tm.action.type == "ADD"
