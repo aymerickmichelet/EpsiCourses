db559c5ab9afd82e07582040a2eced116b74231f
Aymerick Michelet <aymerick.michelet@epsi.fr>
2021-05-04 11:05:44 +0200

    Partie 7

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
 class Question(models.Model):
@@ -11,9 +12,17 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 
 class Choice(models.Model):
