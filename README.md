b527de445fed99979e1342a7561a9ca20a5d5a96
Corentin Lissillour <corentin.lissillour@hotmail.fr>
2021-06-15 15:37:25 +0200

    [ADD] menu

---
+++ README.md
@@ -0,0 +1,12 @@
+#CRYPTO - TP module de chiffrement
+
+Développement d'un module de chiffrement et d'anonymsation des données en Python.
+
+## Setup le projet
+
+Il n'y a rien à installer pour l'instant
+
+## Lancer le programme
+```bash
+python main.py
+```
--- main.py
+++ main.py
@@ -1,6 +1,43 @@
-from anonymasation import anony
+from anonymisation import anony
 from offuscation import offusc
 
-# anony()
+def init():
+    continue_program = True
 
-offusc()
+    print("---------Menu---------")
+    print("1. Anonymisation du fichier")
+    print("2. Offuscation du fichier")
+    print("3. ")
+    print("4. ")
+    print("5. ")
+
+
+    command = input("Veuillez selectionnez votre action : ")
+    actionUser(command)
+
+
+def actionUser(action):
+
+    if action == "1":
+        anony()
+        print("Anonymsation effectué dans le fichier : Liste_des_gagnants_du_Loto_ano.txt")
+    elif action == "2":
+        offusc()
+        print("Offuscation effectué dans le fichier : Liste_des_gagnants_du_Loto_ano.txt")
+    elif action == "3":
+        print("3")
+    elif action == "4":
+        print("4")
+    elif action == "5":
+        print("5")
+    else:
+        print("L'action n'est pas reconnu")
+
+
+if __name__ == "__main__":
+    init()
+
+
+
+
+
